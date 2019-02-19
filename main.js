22ed8093e2dfa8c5a32f51ac4d44c5f93fc1455f
jackajackalop <yixinh@andrew.cmu.edu>
2019-02-19 00:27:55 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+window.addEventListener('load', init, false);
+
+function handleWindowResize(){
+
+}
+
+function init() {
+	createScene();
+	createLights();
+	loop();
+}
+
+function createScene(){
+}
+
+function createLights(){
+
+}
+
+function loop(){
+
+}
